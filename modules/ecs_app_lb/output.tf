ae4037748a1b8c8d928efea6b1e5dd4dd982b464
Łukasz Kostka <lukasz.g.kostka@gmail.com>
2022-09-05 16:06:00 +0200

    Add modules resource outputs

--- modules/ecs_app_lb/output.tf
+++ modules/ecs_app_lb/output.tf
@@ -5,3 +5,11 @@ output "app_role" {
 output "dns_record" {
   value = aws_route53_record.this
 }
+
+output "ecs_service" {
+  value = aws_ecs_service.this
+}
+
+output "target_group" {
+  value = aws_alb_target_group.this
+}
--- modules/ecs_app_nolb/output.tf
+++ modules/ecs_app_nolb/output.tf
@@ -1,3 +1,7 @@
 output "app_role" {
   value = module.ecs_task.app_role
 }
+
+output "ecs_service" {
+  value = aws_ecs_service.this
+}
